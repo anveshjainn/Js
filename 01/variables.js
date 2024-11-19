4f7615dac53dfaae0ed39f9da7700f247ad8827e
anveshjainn <121893909+anveshjainn@users.noreply.github.com>
2024-11-19 18:40:24 +0000

    2nd commit

---
+++ 01/variables.js
@@ -0,0 +1,14 @@
+const accountId = 2345 
+let accountEmail = "anvesh@google.com"
+var accountPassword = 1234
+accountCity = "Bhopal"
+let accountState;
+
+
+accountEmail = "anvesh@gg.com"
+accountPassword = 9876
+accountCity = "Vidisha"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
