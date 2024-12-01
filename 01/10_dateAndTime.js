8be69f06418ee9025ef6ee92c86fd6e943d95bea
anveshjainn <121893909+anveshjainn@users.noreply.github.com>
2024-12-01 23:20:13 +0530

    Update 10_dateAndTime.js

--- 01/10_dateAndTime.js
+++ 01/10_dateAndTime.js
@@ -35,7 +35,7 @@ console.log(specificFormat.toLocaleString());
 // We can also have and check timestamps in js.
 let myTimeStamp = Date.now()
 console.log(myTimeStamp);
-const vcc = 34;
+const vca = 12
 console.log(myTimeStamp.toLocaleString());
 
 
